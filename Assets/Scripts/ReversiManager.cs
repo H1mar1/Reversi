23f59e4de56a3fdff63876c08b1d4660f7cd3647
H1mar1 <1226Himari@au.com>
2025-07-04 22:15:06 +0900

    駒の初期設定

--- Assets/Scripts/ReversiManager.cs
+++ Assets/Scripts/ReversiManager.cs
@@ -8,8 +8,17 @@ public class ReversiManager : MonoBehaviour
     //リバーシの駒を8×8で複製する
     //constで定数を宣言する（後から書き換えることができない）
     const int FIELD_SIZE_X = 8;
-    const int FIELD_SIZE_8 = 8;
+    const int FIELD_SIZE_Y = 8;
 
+    public enum spriteStare
+    {
+        None,//0
+        White,//1
+        Blace,//2
+    }
+
+    private spriteStare[,] _FiledState = new spriteStare[FIELD_SIZE_X, FIELD_SIZE_Y];
+    //private SpriteState[,] _Filed
     private void Start()
     {
         for (int x = 0; x < FIELD_SIZE_X; x++) 
@@ -17,7 +26,19 @@ public class ReversiManager : MonoBehaviour
             for(int y = 0; y < FIELD_SIZE_X; y++)
             {
                 var sprite = Instantiate(reversiGameObjects, new Vector3(-1.122f * x, 0.05f, -1.122f * y), Quaternion.Euler(0, 0, 0));
+
+                _FiledState[x, y] = spriteStare.None;//最初は駒を1つもなしにする
             }
         }
+        //状態保存
+        _FiledState[3, 3] = spriteStare.Blace;
+        _FiledState[3, 4] = spriteStare.White;
+        _FiledState[4, 3] = spriteStare.White;
+        _FiledState[4, 4] = spriteStare.Blace;
+    }
+
+    private void Update()
+    {
+        
     }
 }
